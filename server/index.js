bc1a6247f0dbd1ff13030f9ba6d000e938a6b693
George Andrew Phineas Coon <phineas@Georges-MacBook-Pro.local>
2020-02-06 11:53:45 -0700

    added main and proxy

--- server/index.js
+++ server/index.js
@@ -2,5 +2,9 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
+const PORT = 4040
 
 app.use(express.json())
+app.use(cors())
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
